35a8c1f0d9e833b1b5b79d22a36405d936293a95
son2005 <tranbaoson2005@gmail.com>
2022-10-18 15:14:54 -0400

    Remove unused code

--- app/src/main/java/edu/temple/test/SizeViewModel.kt
+++ app/src/main/java/edu/temple/test/SizeViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 
 class SizeViewModel : ViewModel() {
     private val size = MutableLiveData<Float>()
-    private val text = MutableLiveData<String>()
 
     fun setSize(size: Float) {
         this.size.value = size
